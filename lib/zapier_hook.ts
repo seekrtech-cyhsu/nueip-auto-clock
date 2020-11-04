074c2e9cd7d4b245066c5c790430ebe489062f5f
Cheng-Yu Hsu <cyhsu@seekrtech.com>
2020-11-05 07:39:39 +0800

    Disable email sending

--- lib/zapier_hook.ts
+++ lib/zapier_hook.ts
@@ -1,20 +1,7 @@
 import request from 'request-promise'
 
 async function sendEmail(subject: string, message: string) {
-    const url = process.env.ZAPIER_HOOK_URL
-
-    if (!url) {
-        console.log('Zapier send email trigger is not configured.')
-        return
-    }
-
-    const options = {
-        method: 'POST',
-        uri: url!,
-        body: { subject, message },
-        json: true
-    }
-    return await request.post(options)
+    return 'Skipped email sending'
 }
 
 export default sendEmail
